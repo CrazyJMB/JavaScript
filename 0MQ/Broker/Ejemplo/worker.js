5fbc878b3068929468aa58712cb7446b5b745666
Jorge Martín Barreto <jorma28j@upv.edu.es>
2021-12-02 06:42:24 +0100

    Broker router/dealer -> Acabado

--- 0MQ/Broker/Ejemplo/worker.js
+++ 0MQ/Broker/Ejemplo/worker.js
@@ -4,6 +4,7 @@ let rep = zmq.socket('rep');
 rep.connect('tcp://localhost:9999')
 
 rep.on('message', (msg)=> {
+    for (i = 0; i < msg.length; i++) console.log(String.fromCharCode(msg[i]))
     setTimeout(()=> {
         rep.send('resp')
     }, 1000)
