8bc0532bc1a717837be076902e7df7780ebf30a5
Jorge Martín Barreto <jorma28j@upv.edu.es>
2021-12-02 05:19:03 +0100

    Cliente/Servidor -> Ejemplo muchoAuno

--- 0MQ/Cliente.Servidor/Ejemplos/muchosAuno/cliente1.js
+++ 0MQ/Cliente.Servidor/Ejemplos/muchosAuno/cliente1.js
@@ -2,14 +2,10 @@ const zmq = require('zeromq')
 let s = zmq.socket('req')
 
 s.connect('tcp://127.0.0.1:9998')
-s.connect('tcp://127.0.0.1:9999')
 
-s.send('Alex1')
-s.send('Alex2')
+s.send('uno')
 
-let cont = 0;
 s.on('message', (msg) => {
-    cont++;
     console.log('Recibido: '+msg)
-    if (cont == 2) s.close()
+    s.close()
 })
